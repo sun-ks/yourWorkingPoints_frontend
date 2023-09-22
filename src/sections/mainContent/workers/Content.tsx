748bdc068a9dd637571866e739f7919b47aa4c6b
Igor Storozhuk <igorstorozhuk@MacBook-Pro-Igor.local>
2023-09-22 18:58:42 +0300

    add update worker page

--- src/sections/mainContent/workers/Content.tsx
+++ src/sections/mainContent/workers/Content.tsx
@@ -1,16 +1,11 @@
 import { FC } from "react";
-import {ticketAPI} from "../../../services/TicketService";
 import StyledDataGridUsers from "../../../components/styled/StyledDataGridUsers";
 import { userAPI } from "../../../services/UserService";
 
 const Content: FC = () => {
+  const {data: users, error, isLoading} = userAPI.useGetAllUsersQuery('');
 
-  const {data: tickets, error, isLoading} = ticketAPI.useGetAllItemsTicketsQuery('')
-  const {data: users, error:www, isLoading:eee} = userAPI.useGetAllUsersQuery('');
-
-  console.log(users)
-
-  return <StyledDataGridUsers users={users} error={error} isLoading={isLoading} />
+  return <StyledDataGridUsers users={users} type={"workers"} error={error} isLoading={isLoading} />
 };
 
 export default Content;
