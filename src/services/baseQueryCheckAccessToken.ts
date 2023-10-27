bbdd11ce2c5858b4578ebbe7f9ee4ea0b6986537
Igor Storozhuk <igorstorozhuk@MacBook-Pro-Igor.local>
2023-10-27 10:31:48 +0300

    CheckAccessToken

--- src/services/baseQueryCheckAccessToken.ts
+++ src/services/baseQueryCheckAccessToken.ts
@@ -3,7 +3,11 @@ import type {
   BaseQueryFn,
   FetchArgs,
   FetchBaseQueryError,
-} from '@reduxjs/toolkit/query'
+} from '@reduxjs/toolkit/query';
+import {authSlice} from '../store/reducers/AuthSlice';
+import { Mutex } from 'async-mutex'
+
+const mutex = new Mutex()
 
 const baseUrl = process.env.REACT_APP_API_URL; 
 const baseQueryWithAccessToken = fetchBaseQuery({
@@ -12,27 +16,57 @@ const baseQueryWithAccessToken = fetchBaseQuery({
     const user = (getState() as any);
 
     const accessToken = user.authReducer.user && user.authReducer.user.accessToken;
-   
+
     if (accessToken) {
       headers.set('authorization', `Bearer ${accessToken}`)
     }
+
     return headers
   },
-})
+  credentials: "include"
+});
 
-const baseQueryCheckAccessToken: BaseQueryFn<
+const baseQuery = baseQueryWithAccessToken;
+
+const baseQueryWithReauth: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
+  const {logOut, setCredentials} = authSlice.actions;
 
-  const result = await baseQueryWithAccessToken(args, api, extraOptions);
-  const redirectStatusCode = 403;
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock()
 
-  if (result.error && result.error.status === redirectStatusCode) {
-    window?.location.replace(`/auth/sign_in`);
+  let result = await baseQuery(args, api, extraOptions)
+  
+  if (result.error && result.error.status === 401) {
+    // try to get a new token
+
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
+      
+      try {
+        const refreshResult:any = await baseQuery('/auth/refresh_token', api, extraOptions);
+
+        if (refreshResult.data) {
+          api.dispatch(setCredentials(refreshResult.data));
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions)
+        } else {
+          api.dispatch(logOut(''))
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release()
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock()
+      result = await baseQuery(args, api, extraOptions)
+    }
   }
   return result
 }
 
-export default baseQueryCheckAccessToken;
+export default baseQueryWithReauth;
