f2bb3c5157270cb88330d1649bebf8e0c7423a8b
Igor Storozhuk <igorstorozhuk@MacBook-Pro-Igor.local>
2023-12-28 12:54:47 +0200

    enableMocking only for NODE_ENV=development

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,10 @@ const store = setupStore();
 let persistor = persistStore(store)
 
 async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+
 	const { worker } = await import("./mocks/browser.js");
 	return worker.start();
 }
